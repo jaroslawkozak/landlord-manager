55511bcb364ac459f15e2f844a024f4167f07098
Unknown <jaroslaw.kozak@hotmail.com>
2018-08-22 21:55:17 +0200

    [UI] added initial reactJS project with basic web frame

---
+++ landlord-manager-ui/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <div className="top-bar">
+          aa
+          </div>
+        </header>
+        <div className="contentContainer">
+        content
+        </div>
+        <footer className="footer">
+        foot
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
